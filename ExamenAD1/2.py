270c1adc6b3d35b2b53181661739d6508f4d5ab2
Mateo Cevallos <66786471+matojo074@users.noreply.github.com>
2021-07-30 20:57:13 -0500

    Add files via upload

---
+++ ExamenAD1/2.py
@@ -0,0 +1,45 @@
+import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+###API ########################
+consumer_key = "irnK8KT1RrNyV9OUQDHwQ5Icx"
+consumer_secret = "IuMMfp8DM5uFjOJlu5iceDnqkzK2CkS7PkBTbTBRNR7Upgq5mP"
+access_token = "784853519497105408-8OXTILd2D1ppzvQf1jO9LY8L8iuyyZc"
+access_token_secret = "vpsfDZIzs7JcQbLo27odaSxST3MbQEsDvypkwqvlduiKd"
+
+#####################################
+class listener(StreamListener):
+
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = db.save(dictTweet)
+            print("SAVED" + str(doc) + "=>" + str(data))
+        except:
+            print("Already exists")
+            pass
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitterStream = Stream(auth, listener())
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://admin:FARAaM12@127.0.0.1:5984')
+try:
+    db = server.create('jolimpicos')
+except:
+    db = server['jolimpicos']
+
+'''===============LOCATIONS=============='''
+
+twitterStream.filter(locations=[-83.6,-56.2,-34.3,14.9])
+twitterStream.filter(track=['JuegosOlimpios'])
